63be643a4ebb1b6d4dc5895fac6a0f5c1f84dbc1
Justyna Wojtczak <justine84@gmail.com>
2015-10-11 00:16:47 +0200

    Hide help link + move administration to the end

--- init.rb
+++ init.rb
@@ -12,11 +12,16 @@ end
 
 #end
 
-Rails.application.config.to_prepare do
+Rails.application.config.to_prepare do  
   project_id = Setting['plugin_redmine_contract_management']['contract_management_project']
   if project_id.present?
   	project_name = Project.find(project_id).try(:name)
   	Redmine::MenuManager.map(:top_menu)
   						.push(:project, { :controller => 'projects', :action => 'show', id: project_id }, :last => true,  :caption => project_name)
   end
+  Redmine::MenuManager.map(:top_menu).delete(:help)
+
+  # move administration link to end
+  Redmine::MenuManager.map(:top_menu).delete :administration
+  Redmine::MenuManager.map(:top_menu).push :administration, { :controller => 'admin', :action => 'index' }, :if => Proc.new { User.current.admin? }, :last => true
 end
